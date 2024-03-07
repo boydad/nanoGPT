78ac7b462ac0f5d09d54e9c8a98138b3ae6a8798
Denis Boyda <boyda@polaris-login-03.hsn.cm.polaris.alcf.anl.gov>
2024-03-07 18:03:43 +0000

    Add filedir for configurator and data

--- bench.py
+++ bench.py
@@ -26,7 +26,7 @@ device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = True # use PyTorch 2.0 to compile the model to be faster
 profile = False # use pytorch profiler, or just simple benchmarking?
-exec(open('configurator.py').read()) # overrides from command line or config file
+exec(open(os.path.dirname(__file__)+'/configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
 torch.manual_seed(seed)
@@ -48,7 +48,7 @@ if ddp:
 # data loading init
 if real_data:
     dataset = 'openwebtext'
-    data_dir = os.path.join('data', dataset)
+    data_dir = os.path.join(os.path.dirname(__file__) + '/data', dataset)
     train_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
     def get_batch(split):
         data = train_data # note ignore split in benchmarking script
